2a819c31341f6c245c62a003491991e99d5003d0
SERVER\mauricio <mauriciocarvalhoac@gmail.com>
2026-01-20 22:30:44 -0300

    primeiro commit do site

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>MRC Web Project</title>
+</head>
+
+<body>
+	<h3>Esse site está em Desenvolvimento</h3>
+</body>
+</html>
